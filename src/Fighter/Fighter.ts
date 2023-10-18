1cababf102acf7a9418279807d4b36745e8ea76f
Francisco Grande <fjgrandv@gmail.com>
2023-10-18 19:34:27 +0200

    Add fighter class

---
+++ src/Fighter/Fighter.ts
@@ -0,0 +1,29 @@
+import Character from "../Character/Character";
+import type { FighterData } from "../type/type";
+
+export class Fighter extends Character {
+  weapon;
+  dexterity;
+
+  constructor(data: FighterData) {
+    super(data);
+    this.weapon = data.weapon;
+    this.dexterity = data;
+  }
+
+  communicate(): string {
+    return `${super.communicate()}First I hit and then I ask`;
+  }
+
+  private filterDexterity(dexterity: number) {
+    if (dexterity < 0) {
+      return 0;
+    }
+
+    if (dexterity > 10) {
+      return 10;
+    }
+
+    return dexterity;
+  }
+}
