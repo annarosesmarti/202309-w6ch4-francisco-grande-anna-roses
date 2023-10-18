88b66838a6d90cc969acb9a1bffce5fd4e73fa6f
Francisco Grande <fjgrandv@gmail.com>
2023-10-19 01:09:08 +0200

    Add class App

---
+++ src/App/App.ts
@@ -0,0 +1,8 @@
+import Component from "../components/component.js";
+
+class App extends Component {
+  populate(): void {
+    throw new Error("Mehod not implemented");
+  }
+}
+export default App;
