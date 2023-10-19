81e685bdea0620ddd130be8237c0198ce099e50c
Anna Roses <anna.roses.marti@gmail.com>
2023-10-19 19:52:32 +0200

    Refactor in constructor and add protection

--- src/components/Advisor/Advisor.ts
+++ src/components/Advisor/Advisor.ts
@@ -1,15 +1,20 @@
 import Character from "../Character/Character";
 import type { AdvisorData } from "../../types";
 
-export class Adviser extends Character {
+class Adviser extends Character {
   adviseCharacter;
 
   constructor(data: AdvisorData) {
     super(data);
-    this.adviseCharacter = data.adviseCharacter;
+
+    if (data.adviseCharacter instanceof Character) {
+      this.adviseCharacter = data.adviseCharacter;
+    }
   }
 
-  communicate(): string {
-    return "No sé por qué, pero creo que voy a morir pronto";
+  protected communicate(): string {
+    return `${super.communicate()}I have a funny feeling that I'm going to die`;
   }
 }
+
+export default Adviser;
